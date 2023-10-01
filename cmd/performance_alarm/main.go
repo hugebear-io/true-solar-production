27bec62c31d753c28bf11fac3440613574a66665
tkthanatorn <tk.thanatorn@gmail.com>
2023-10-01 19:51:18 +0700

    chore: chage aggs size

--- cmd/performance_alarm/main.go
+++ cmd/performance_alarm/main.go
@@ -25,7 +25,7 @@ func main() {
 	}
 
 	compositeAggregation := elastic.NewCompositeAggregation().
-		Size(10000).
+		Size(10).
 		Sources(elastic.NewCompositeAggregationDateHistogramValuesSource("date").Field("@timestamp").CalendarInterval("1d").Format("yyyy-MM-dd"),
 			elastic.NewCompositeAggregationTermsValuesSource("vendor_type").Field("vendor_type.keyword"),
 			elastic.NewCompositeAggregationTermsValuesSource("id").Field("id.keyword")).
@@ -63,5 +63,58 @@ func main() {
 		panic(err)
 	}
 
-	util.PrintJSON(result)
+	util.PrintJSON(map[string]interface{}{"result": result})
+
+	// if result.Aggregations == nil {
+	// 	panic("no aggregation")
+	// }
+
+	// performanceAlarmAgg, found := result.Aggregations.Composite("performance_alarm")
+	// if !found {
+	// 	panic("no performance_alarm aggregation")
+	// }
+
+	// items := make([]*elastic.AggregationBucketCompositeItem, 0)
+	// items = append(items, performanceAlarmAgg.Buckets...)
+
+	// if len(performanceAlarmAgg.AfterKey) > 0 {
+	// 	afterKey := performanceAlarmAgg.AfterKey
+
+	// 	for {
+	// 		query := e.Search("solarcell*").
+	// 			Size(0).
+	// 			Query(elastic.NewBoolQuery().Must(
+	// 				elastic.NewMatchQuery("data_type", constant.DATA_TYPE_PLANT),
+	// 				elastic.NewRangeQuery("@timestamp").Gte("now-7d/d").Lte("now-1d/d"),
+	// 			)).
+	// 			Aggregation("performance_alarm", compositeAggregation.AggregateAfter(afterKey))
+
+	// 		result, err := query.Pretty(true).Do(context.Background())
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+
+	// 		if result.Aggregations == nil {
+	// 			panic("no aggregation")
+	// 		}
+
+	// 		performanceAlarmAgg, found := result.Aggregations.Composite("performance_alarm")
+	// 		if !found {
+	// 			panic("no performance_alarm aggregation")
+	// 		}
+
+	// 		items = append(items, performanceAlarmAgg.Buckets...)
+
+	// 		if len(performanceAlarmAgg.AfterKey) == 0 {
+	// 			break
+	// 		}
+
+	// 		afterKey = performanceAlarmAgg.AfterKey
+	// 	}
+	// }
+
+	// x, found := result.Aggregations.Composite("performance_alarm")
+	// if found {
+	// 	util.PrintJSON(map[string]interface{}{"result": x})
+	// }
 }
