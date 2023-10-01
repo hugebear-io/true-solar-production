cbc3af82db022537e15c90fbf9c34462e4482998
tkthanatorn <tk.thanatorn@gmail.com>
2023-10-01 19:59:39 +0700

    chore: try to query afterKey

--- cmd/performance_alarm/main.go
+++ cmd/performance_alarm/main.go
@@ -76,6 +76,37 @@ func main() {
 	items = append(items, performanceAlarm.Buckets...)
 	util.PrintJSON(map[string]interface{}{"result": items, "after_key": performanceAlarm.AfterKey})
 
+	if len(performanceAlarm.AfterKey) > 0 {
+		afterKey := performanceAlarm.AfterKey
+
+		for {
+			query := e.Search("solarcell*").
+				Size(0).
+				Query(elastic.NewBoolQuery().Must(
+					elastic.NewMatchQuery("data_type", constant.DATA_TYPE_PLANT),
+					elastic.NewRangeQuery("@timestamp").Gte("now-7d/d").Lte("now-1d/d"),
+				)).
+				Aggregation("performance_alarm", compositeAggregation.AggregateAfter(afterKey))
+
+			result, err := query.Pretty(true).Do(context.Background())
+			if err != nil {
+				panic(err)
+			}
+
+			if result.Aggregations == nil {
+				panic("no aggregation")
+			}
+
+			performanceAlarm, found := result.Aggregations.Composite("performance_alarm")
+			if !found {
+				panic("no performance_alarm aggregation")
+			}
+
+			items = append(items, performanceAlarm.Buckets...)
+			break
+		}
+	}
+	util.PrintJSON(map[string]interface{}{"result": items, "after_key": performanceAlarm.AfterKey})
 	// if len(performanceAlarmAgg.AfterKey) > 0 {
 	// 	afterKey := performanceAlarmAgg.AfterKey
 
